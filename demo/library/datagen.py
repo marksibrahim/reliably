53e3ea2f2db96eac358e394b2d74bd24d7689771
Ceena Modarres <ceenam91@gmail.com>
2018-04-05 23:41:16 -0400

    Change directory structure to include library directory

---
+++ demo/library/datagen.py
@@ -0,0 +1,94 @@
+import numpy as np
+from typing import List, Dict, NewType
+import datetime
+
+
+class DataGen:
+    def __init__(self):
+        """
+        initialize type annotations and data structures
+        """
+        self.sensor_dict = {}
+        self.batch_list = []
+        self.timestamp = datetime.datetime.now()
+        self.vibration_sensor: float
+        self.flow: float
+        self.pressure: float
+        self.power_consumption: float
+        self.operational: bool
+
+    def __flow_relationship(self, vibration: float, pressure: float) -> float:
+        """
+        construct arbitrary relationship between vibration, pressure, and flow
+
+        :param vibration: float
+        :param pressure: float
+        :return: flow: float
+        """
+        if self.timestamp.minute < 30:
+            flow = 3.*vibration + 3.*pressure + np.random.normal(0,.5,1)[0]
+
+        else:
+            flow = 12.*vibration + 3.*pressure + np.random.normal(0,.5,1)[0]
+
+        return flow
+
+    def __power_gen(self) -> float:
+        """
+        create power consumption variable
+
+        :return: float
+        """
+        power_consumption = -1.
+
+        if self.timestamp.minute < 40:
+            power_consumption = np.random.normal(83, 10, 1)[0]
+
+        elif 40 <= self.timestamp.minute <= 55:
+            power_consumption = np.random.normal(83,20,1)[0]
+
+        else:
+            power_consumption = 0.
+
+        if power_consumption < 0.:
+            power_consumption = 0.
+
+        return power_consumption
+
+    def dict_gen(self) -> Dict:
+        """
+        Generates a dictionary corresponding to a single sensor reading.
+
+        :return: dictionary of attributes collected from sensor
+        """
+        self.sensor_dict['timestamp'] = self.timestamp
+        self.sensor_dict['vibration_sensor'] = np.random.normal(8,2,1)[0]
+        self.sensor_dict['pressure'] = np.random.normal(1,.1,1)[0]
+        self.sensor_dict['flow'] = self.__flow_relationship(self.sensor_dict['vibration_sensor'],
+                                                       self.sensor_dict['pressure']) # Convert to property?
+        self.sensor_dict['power_consumption'] = self.__power_gen()
+
+        if 0 < self.sensor_dict['power_consumption']:
+            self.sensor_dict['operational'] = True
+        else:
+            self.sensor_dict['operational'] = False
+
+        return self.sensor_dict
+
+    def batch_generator(self, batch_size: int) -> List[Dict]:
+        """
+        Generates a list of dictionaries which corresponds to collecting significant data at once.
+
+        :param batch_size: int, quantity of sensor observations in list
+        :return: list[dict] full batches of data collected from sensors
+        """
+        for _ in batch_size:
+            self.batch_list.append(self.dict_gen())
+
+        return self.batch_list
+
+
+if __name__ == "__main__":
+    DG = DataGen()
+    ob = DG.dict_gen()
+    print(ob)
