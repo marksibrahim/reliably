6ba708dd4d8139a952bdbe34138629251ecfe45b
Ceena Modarres <ceenam91@gmail.com>
2018-04-05 21:23:30 -0400

    Include SQL Database Insertion

--- demo/sqldb.py
+++ demo/sqldb.py
@@ -1,13 +1,36 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Float, String, DateTime, Boolean
 from sqlalchemy_utils import database_exists, create_database
-
+from typing import Dict
+import datetime
 
 class DatabaseWriter:
 
     def __init__(self, engine):
         self.engine = engine
 
-    def create_db(self):
+    def data_insertion(self, data_dict: Dict):
+        """
+        Insert data into schema. Note, this doesn't currently support batches.
+
+        :param data_dict: data to insert into system schema
+        :return: None
+        """
+
+        self.__create_db()
+        self.__create_table()
+
+        self.current_state = self.system.insert().values(
+            timestamp = data_dict['timestamp'],
+            vibration_sensor = data_dict['vibration_sensor'],
+            flow = data_dict['pressure'],
+            pressure = data_dict['flow'],
+            power_consumption = data_dict['power_consumption'],
+            operational = data_dict['operational']
+        )
+
+        self.connection.execute(self.current_state)
+
+    def __create_db(self):
         """
         Initialize DB if necessary
 
@@ -21,7 +44,7 @@ class DatabaseWriter:
 
         return "Does DB exist? {}".format(database_exists(self.db.url))
 
-    def create_table(self):
+    def __create_table(self):
         """
         Initialize DB Table if necessary
 
@@ -31,7 +54,7 @@ class DatabaseWriter:
         self.connection = self.db.connect()
         self.metadata = MetaData(self.connection)
 
-        system = Table('system_monitoring', self.metadata,
+        self.system = Table('system_monitoring', self.metadata,
                        Column('timestamp', DateTime(), primary_key=True, nullable=False),
                        Column('vibration_sensor', Float()),
                        Column('flow', Float()),
@@ -43,6 +66,5 @@ class DatabaseWriter:
         self.metadata.create_all()
 
 if __name__ == "__main__":
-    dbw = DatabaseWriter(engine="postgresql://reliably_ai@localhost:5432/sample_schema")
-    dbw.create_db()
-    dbw.create_table()
+    dbw = DatabaseWriter(engine="postgresql://reliably_ai@localhost:5432/sample_schema_demo")
+    dbw.data_insertion(temp_dict)
