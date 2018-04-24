e4115b070663300f0f8c2068750787b00bce0a05
marksibrahim <mark.ibhm@gmail.com>
2018-04-24 17:37:46 -0400

    dashboard now served via flask with static css

--- demo/app.py
+++ demo/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "Reliably is born."
+
+@app.route("/")
+def dashboard():
+    return render_template("index.html")
+
 
 
 if __name__ == "__main__":
